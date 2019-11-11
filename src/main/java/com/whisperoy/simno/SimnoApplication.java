482f95cb2d6a2bd391c93cb4d593dadb508402db
rongyuan <rongyuan@tsign.cn>
2019-11-11 11:53:39 +0800

    init

---
+++ src/main/java/com/whisperoy/simno/SimnoApplication.java
@@ -0,0 +1,13 @@
+package com.whisperoy.simno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimnoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimnoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whisperoy/simno/SimnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whisperoy.simno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimnoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
